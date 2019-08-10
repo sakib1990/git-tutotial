2c84e0825adb6c9496ce8aa0d3c930b3a866e95d
sakib <ahmadsakibabdullah@gmail.com>
2019-08-10 14:53:20 +0600

    Added a new method called display tp Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void mainstring[] args) {
         system.out.orintln("Vs code is Awesome....");
+        greeting("sakib")
     }
+}
+
+
+public static void greeting(string name) {
+    system.out.println("welcome " + name);
 }
